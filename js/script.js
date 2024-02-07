b8da9d0d10b40feb6aeacdb4bbeb3c221b598569
emanuele-pogliari <manu.pogliari@gmail.com>
2024-02-07 22:24:32 +0100

    exercise completed with bonus

--- js/script.js
+++ js/script.js
@@ -2,20 +2,56 @@ const btnGen = document.querySelector("#btn-gen-game");
 const gridElement = document.querySelector("#grid")
 
 btnGen.addEventListener("click", genGrid);
-
+let newElement;
 function genGrid() {
     gridElement.innerHTML = "";
-    for (let i = 0; i < 100; i++) {
-        const newElement = document.createElement("div");
+    let num = modifyDifficulty();
+
+    for (let i = 0; i < num; i++) {
+        newElement = document.createElement("div");
         newElement.classList.add("square");
         newElement.innerText = i + 1;
+        addClass(newElement, num);
 
         newElement.addEventListener("click", function () {
 
             console.log(this.innerText);
             this.classList.toggle("clicked");
         });
-
         gridElement.append(newElement);
+    }
+}
+
+
+function modifyDifficulty() {
+    let numberSquares = 0;
+    let valueSquare = document.querySelector("#select-difficulty").value;
+    if (valueSquare == "1") {
+        numberSquares = 49;
+    }
+    if (valueSquare == "2") {
+        numberSquares = 81;
+    }
+    if (valueSquare == "3") {
+        numberSquares = 100;
+    }
+    return numberSquares;
+}
+
+
+function addClass(changeGrid, num) {
+    let newClass = "a"
+    if (num == 49) {
+    }
+    if (num == 81) {
+        newClass = "squareMedium";
+        changeGrid.classList.add(newClass);
+        changeGrid.classList.remove("square");
+    }
+    else if (num == 100) {
+
+        newClass = "squareHard";
+        changeGrid.classList.add(newClass);
+        changeGrid.classList.remove("square");
     }
 }
